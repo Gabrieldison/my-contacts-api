1ec09b3bbaa547e687a57b483cd4d4a11f643bd9
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2022-11-28 19:06:27 -0300

    feat: create api of contacts

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+require("express-async-errors");
+
+const routes = require("./routes");
+
+const app = express();
+
+const port = 3000;
+app.use(express.json());
+app.use(routes);
+app.use((error, request, response, next) => {
+  console.log("### ERROR HANDLER");
+  console.log(error);
+  response.sendStatus(500);
+});
+
+app.listen(port, () =>
+  console.log(`Server started at http://localhost:${port}`)
+);
